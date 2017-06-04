4d262fb3eea88bbc37397ea14bb1d877ea31a343
João Carlos <joaocarlos422@gmail.com>
2017-06-04 02:37:15 -0300

    Melhorando interface

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,6 @@
 </head>
 <h1>txt2img</h1>
 <form action="upload" method="post" enctype="multipart/form-data">
-    Select image to upload:
     <input type="file" name="file" id="file">
     <input type="submit" value="Upload Image" name="submit">
 </form>
