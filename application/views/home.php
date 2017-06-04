a6ef70ff1ffb2010129aec5be0401d6f0754bca9
João Carlos <joaocarlos422@gmail.com>
2017-06-04 02:30:53 -0300

    Melhorando interface

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,7 @@
+<head>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<h1>txt2img</h1>
 <form action="upload" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="file" id="file">
